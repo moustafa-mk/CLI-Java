67e0231a306d642f9ac3717bd3eadd9616a5b02c
Moustafa <moustafa.mkms@gmail.com>
2019-10-13 21:59:16 +0200

    Added main method

--- src/CLI/Terminal.java
+++ src/CLI/Terminal.java
@@ -4,37 +4,35 @@ import java.io.*;
 
 public class Terminal {
     private String currDirectory;
-    Terminal()
-    {
+
+    Terminal() {
         currDirectory = "C:\\";
     }
+
     public void cp(String SourthPath, String DestinationPath) throws IOException // Didn't check if they already have directory or not, need the path helper func
     {
-        File file = new File(currDirectory+SourthPath);
-        if(!file.exists())
-        {
+        File file = new File(currDirectory + SourthPath);
+        if (!file.exists()) {
             System.out.println("Couldn't find this file/directory");
             return;
         }
-        file = new File(currDirectory+DestinationPath);
-        if(!file.exists())
-        {
+        file = new File(currDirectory + DestinationPath);
+        if (!file.exists()) {
             Boolean check = file.createNewFile();
-            if(check)
-            {
+            if (check) {
                 System.out.println("Second file didn't exit, creating file now");
-            }
-            else{
+            } else {
                 System.out.print("Failed to create file");
                 return;
             }
         }
-        FileReader fr = new FileReader(currDirectory+SourthPath);
+        FileReader fr = new FileReader(currDirectory + SourthPath);
         BufferedReader br = new BufferedReader(fr);
-        FileWriter fw = new FileWriter(currDirectory+ DestinationPath,true);
+        FileWriter fw = new FileWriter(currDirectory + DestinationPath, true);
         String s;
-        while((s=br.readLine())!=null){
-            fw.write(s); fw.flush();
+        while ((s = br.readLine()) != null) {
+            fw.write(s);
+            fw.flush();
         }
     }
 }
--- src/CLI/main.java
+++
@@ -1,5 +0,0 @@
-package CLI;
-
-public class main {
-
-}
